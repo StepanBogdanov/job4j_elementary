f3990acea72144d5e7b77b1ce1bee312420a0fa6
Stepan Bogdanov <bogstepan@gmail.com>
2021-01-20 22:35:33 +0300

    6.0.2. Заполнение массива. [#448243]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива surnames равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Ivan Bobrov";
+        names[1] = "Alexander Plate";
+        names[2] = "Sergey Kozlov";
+        names[3] = "Valeriy Zhdanov";
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
